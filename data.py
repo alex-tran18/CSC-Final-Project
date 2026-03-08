79a9019ad23842239b8777ed5007702b4a1e9b5c
audrey-choi <achoi79@calpoly.edu>
2026-03-08 04:17:11 -0700

    ryt

---
+++ data.py
@@ -0,0 +1,26 @@
+class User:
+    # Initializes a new User object
+    # input: the user's ID as an integer
+    # input: the total time spent on social media(in hours per month) as a float or integer
+    # input: productivity loss score as a float or integer
+    # input: watch reason as a string
+    # input: debt status as a boolean
+    def __init__(self, user_id, total_time_spent, productivity_loss, watch_reason,, debt):
+        self.user_id = user_id
+        self.total_time_spent = total_time_spent
+        self.productivity_loss = productivity_loss
+        self.watch_reason = watch_reason
+        self.debt = debt
+
+    #Provide a developer-friendly string representation of the object User.
+    # input: User object for which a string representation is desire
+    # output: string representation
+    def __repr__(self):
+        return 'User({}, {}, {}, {}, {}'.format(
+            self.user_id,
+            self.total_time_spent,
+            self.productivity_loss,
+            self.watch_reason,
+            self.debt
+        )
+
