186b6ec44acb5398a9b9b71d96028aa92e07063e
Ch1llaxer <olgetharth0129@gmail.com>
2026-03-08 06:27:46 -0700

    data

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import math
 import data
 from data import User
 
-def
+# Convert Debt String into Boolean
+def convert_bool(tf:str) -> bool:
+    tf = str(tf).strip().lower()
+    if tf == "true":
+        return True
+    elif tf == "false":
+        return False
