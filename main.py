0370d637417ca06009291888b11924c977342f6e
Ch1llaxer <olgetharth0129@gmail.com>
2026-03-08 07:35:57 -0700

    Main.py setup

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import csv
-import math
-import data
 from data import User
 
 # Convert Debt String into Boolean
-def convert_bool(tf:str) -> bool:
-    tf = str(tf).strip().lower()
-    if tf == "true":
-        return True
-    elif tf == "false":
-        return False
+    def convert_bool(tf:str) -> bool:
+        tf = str(tf).strip().lower()
+        if tf == "true":
+            return True
+        elif tf == "false":
+            return False
+        else:
+            return False
 
     # Load the CSV File
     def load_users(filename):
