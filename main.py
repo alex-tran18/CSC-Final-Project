5845333ca87b6c8f91050323f6ac1a5c550ab95e
Ch1llaxer <olgetharth0129@gmail.com>
2026-03-09 04:32:45 -0700

    update

--- main.py
+++ main.py
@@ -264,7 +264,7 @@ def main():
     print("Ratio (Both / Debt users):", round(ratio, 3))
     print()
 
-    # Correlation between screen time and debta
+    # Correlation between screen time and debt
     corr = correlation_time_and_debt(users)
 
     print("Correlation (Screen Time vs Debt):", round(corr, 3))
